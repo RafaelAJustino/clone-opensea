1c4f9fff9620ce98811efa970aa770c5b00654bb
RafaelAJustino <rafael.angelo.justino@gmail.com>
2022-12-09 21:33:44 -0300

    implementacao projeto 3

--- script/scripts.js
+++ script/scripts.js
@@ -2,6 +2,7 @@
 const burguerMenu = document.querySelector('.hamburguer');
 const navMenu = document.querySelector('.nav-menu-mobile');
 const erLogin = document.getElementById('er-login');
+const erCadastro = document.getElementById('er-cadastro');
 const srcInput = document.getElementById('src-input');
 const div1 = document.querySelector('.div-1');
 const div2 = document.querySelector('.div-2');
@@ -11,8 +12,6 @@ const secItems = document.getElementById('character');
 const pages = 42;
 const src = document.getElementById('searchIp');
 
-
-
 const menuIsActive = () => {
 	burguerMenu.classList.toggle('active');
 	navMenu.classList.toggle('hidden')
@@ -21,34 +20,92 @@ const menuIsActive = () => {
 burguerMenu.addEventListener('click', menuIsActive);
 
 async function search() {
-	const src = document.getElementById('searchIp').value;
-	console.log(src);
-	fetch(`https://rickandmortyapi.com/api/character/?name=${src}`)
+	const message = document.getElementById('searchIp').value;
+	const data = {message};
+
+	fetch(`http://localhost:3000/user-post/get`, {
+		method: 'post',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(data)
+	})
 		.then((response) =>
 			response.json())
 		.then(async (data) => {
-			const result = data.results;
+			console.log(data)
+			const result = data;
 			secItems.innerText = '';
-			result.forEach((product) => {
-				const img = document.createElement('img');
-				img.src = product.image;
-				secItems.appendChild(img);
+			result.forEach((item, i) => {
+				const p = document.createElement('p');
+				p.innerText = `${i + 1}: ${item.message}`;
+				secItems.appendChild(p);
 			});
 		});
+}
+
+async function cadastrar() {
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	const data = { email, password }
+
+	if (!email.includes('@')) {
+		erCadastro.classList.remove('hidden');
+	} else {
+
+		await fetch('http://localhost:3000/user/create', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)
+		}).then((response) => response.json())
+			.then((data) => {
+				console.log('aaaa', data.token);
+				if (!!data.message) {
+					alert(data.message);
+				} else {
+					alert(data.error)
+				}
+			})
+	}
+}
+
+async function publicar() {
+	const message = document.getElementById('post').value;
+	console.log(message);
+	const data = { message }
+
+	if (message.length < 10) {
+		alert('ERRO!\nPublicação deve conter no mínimo 10 caracteres!');
+	} else {
+
+		await fetch('http://localhost:3000/user-post/create', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)
+		}).then((response) => response.json())
+			.then((data) => {
+				console.log('aaaa', data.message);
+				if (!!data.message) {
+					alert(data.message);
+					document.location.reload(true);
+				} else {
+					alert(data.error)
+				}
+			})
+	}
 }
 
 async function login() {
 	const email = document.getElementById('email').value;
 	const password = document.getElementById('password').value;
+	const data = { email, password }
 
 	if (email.length < 3 || password.length < 3) {
 		erLogin.classList.remove('hidden');
 	} else {
-		const data = `email=${email}&password=${password}`
-		await fetch("https://reqres.in/api/login", {
+
+		await fetch('http://localhost:3000/user/login', {
 			method: 'post',
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-			body: data
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)
 		}).then((response) => response.json())
 			.then((data) => {
 				console.log('aaaa', data.token);
@@ -62,22 +119,7 @@ async function login() {
 				} else {
 					erLogin.classList.remove('hidden');
 				}
-			});
-
-			for (let i = 1; i < pages; i++) {
-				fetch(`https://rickandmortyapi.com/api/character/?page=${i}`)
-					.then((response) =>
-						response.json())
-					.then(async (data) => {
-						const result = data.results;
-						result.forEach((product) => {
-							const opt = document.createElement('option');
-							opt.value = product.name;
-							opt.innerText = product.name;
-							src.appendChild(opt);
-						});
-					});
-			}
+			})
 	}
 }
 
@@ -85,20 +127,20 @@ window.onload = async () => {
 	const hasToken = localStorage.getItem('reqres');
 
 	if (!!hasToken) {
-		for (let i = 1; i < pages; i++) {
-			fetch(`https://rickandmortyapi.com/api/character/?page=${i}`)
-				.then((response) =>
-					response.json())
-				.then(async (data) => {
-					const result = data.results;
-					result.forEach((product) => {
-						const opt = document.createElement('option');
-						opt.value = product.name;
-						opt.innerText = product.name;
-						src.appendChild(opt);
-					});
+
+		fetch(`http://localhost:3000/user-post/get-all`)
+			.then((response) =>
+				response.json())
+			.then(async (data) => {
+				console.log(data)
+				const result = data;
+				secItems.innerText = '';
+				result.forEach((item, i) => {
+					const p = document.createElement('p');
+					p.innerText = `${i + 1}: ${item.message}`;
+					secItems.appendChild(p);
 				});
-		}
+			});
 
 		srcInput.classList.remove('hidden');
 		div1.classList.remove('hidden');
